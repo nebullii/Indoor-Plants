cf09bc337d4f08e9a9c6f03f0c156cc7c8e69470
Nebullii <nebullii@icloud.com>
2024-11-11 22:03:57 -0500

    Minor Changes

--- build.sh
+++ build.sh
@@ -9,4 +9,6 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Apply database migrations
-python manage.py migrate
+python manage.py makemigrations products
+python manage.py migrate --noinput
+python manage.py migrate --run-syncdb
