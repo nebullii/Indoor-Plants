accccd89d2b8191011d2014e82591aef3c0f6f0d
Neha Chaudhari <nehachaudhari@Nehas-Air-3.lan>
2025-03-29 20:13:24 -0400

    Test webhook deployment

--- indoor_plant/urls.py
+++ indoor_plant/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from products.views import home_view
+from .views import update_from_github
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,6 +12,7 @@ urlpatterns = [
     path('', home_view, name='home'),
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
+    path('update_from_github/', update_from_github, name='update_from_github'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Add static file serving for development
