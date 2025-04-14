8e0442d221bca1b769d55ba282e64c6d63c290db
nebullii <nchaudhari@clarku.edu>
2025-04-14 16:09:46 -0400

    Update cart template: - Add black text color throughout - Make product images and names clickable - Add back to home button - Improve delete functionality with animations

--- cart/views.py
+++ cart/views.py
@@ -38,8 +38,11 @@ def add_to_cart(request, product_id):
         cart_item.quantity += 1
         cart_item.save()
     
-    messages.success(request, f'{product.name} added to cart.')
-    return redirect('cart:view_cart')
+    return JsonResponse({
+        'success': True,
+        'message': f'{product.name} added to cart successfully!',
+        'cart_count': cart.get_total_items()
+    })
 
 @login_required
 def update_cart(request, item_id):
