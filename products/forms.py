7c9bc61780bfba01a656f01f0a48b9ddec86ee26
nebullii <nchaudhari@clarku.edu>
2025-07-09 15:24:46 -0400

    Minor Fix with updated product page

--- products/forms.py
+++ products/forms.py
@@ -5,7 +5,7 @@ from .models import Product
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = '__all__'
+        exclude = ['seller']
         widgets = {
             'tags': forms.SelectMultiple(attrs={'class': 'form-select'}),
         }
